bc527edcb4f79229a059107ca53194e00b43d7d9
Konrad Jaroslawski <kgj.mails@gmail.com>
2020-07-01 11:25:45 +0200

    Init

---
+++ src/main/java/eu/qamar/brewery/TsbbEqBreweryApplication.java
@@ -0,0 +1,13 @@
+package eu.qamar.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbbEqBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsbbEqBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/qamar/brewery/TsbbEqBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.qamar.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbbEqBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
